0dafaafe170a8730120282c16deb9b20dfb99de7
thefubon <43171309+thefubon@users.noreply.github.com>
2021-10-12 22:26:30 +0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen bg-gray-100">
+    <slot />
+  </div>
+</template>
+
+
+<style lang="postcss">
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="min-h-screen flex flex-col justify-center items-center">
+    <nuxt-link
+      to="/test"
+      class="
+        bg-indigo-100
+        text-indigo-600
+        px-4
+        py-2
+        max-w-md
+        rounded-lg
+        text-center
+        shadow
+      "
+    >
+      Go to Test page
+    </nuxt-link>
+  </div>
+</template>
