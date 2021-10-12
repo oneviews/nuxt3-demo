797331c218527b5d0e3326780cdfec5928babb64
thefubon <43171309+thefubon@users.noreply.github.com>
2021-10-12 22:47:17 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-screen flex flex-col justify-center items-center">
+  <div class="min-h-screen flex justify-center items-center space-x-4">
     <nuxt-link
       to="/test"
       class="
@@ -13,7 +13,19 @@
         shadow
       "
     >
-      Go to Test page
+      Test Page
     </nuxt-link>
+
+    <a class="
+      bg-red-100
+        text-red-600
+        px-4
+        py-2
+        max-w-md
+        rounded-lg
+        text-center
+        shadow" href="/api/hello">
+        Api (Hello)
+    </a>
   </div>
 </template>
